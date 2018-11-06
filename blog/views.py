71544aae935aab9821707987ad298d12068a3f13
samhaldia <samhaldia123@gmail.com>
2018-11-06 14:30:09 +0530

    added tests and updated views

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.urls import reverse_lazy
 
 # blog/views.py
 from django.views.generic import ListView, DetailView
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from . models import Post
 
@@ -18,3 +21,23 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 	
+	
+	
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+	
+	
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+	
+	
+	
+	
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
